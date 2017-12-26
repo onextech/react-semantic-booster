5b4999c3458e8d86a1165e608b8c5281ff3a0fa3
clodal <joel.koh@me.com>
2017-12-26 13:26:04 +0800

    added enyzme, react-test-renderer configured jest for component unit testing added basic testsuite for ButtonLink component

---
+++ src/components/atoms/ButtonLink/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, render } from 'enzyme';
+import { MemoryRouter as Router, Link } from 'react-router-dom';
+import ButtonLink from './index';
+
+
+describe('<ButtonLink>', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Router><ButtonLink to="http://www.facebook.com">Facebook</ButtonLink></Router>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('has link child', () => {
+    const wrapper = shallow(<ButtonLink to="/url" />);
+    expect(wrapper.find(Link).length).toBe(1);
+  });
+
+  it('has semantic button class', () => {
+    const wrapper = render(<Router><ButtonLink to="/url" /></Router>);
+    expect(wrapper.hasClass('ui')).toBe(true);
+  });
+});
