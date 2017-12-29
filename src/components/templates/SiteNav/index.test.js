2eeb92798aee75e1d072712a9dabf1181bb853f6
clodal <joel.koh@me.com>
2017-12-29 17:31:17 +0800

    fixed lint and tests for SiteNav component

--- src/components/templates/SiteNav/index.test.js
+++ src/components/templates/SiteNav/index.test.js
@@ -3,19 +3,21 @@ import renderer from 'react-test-renderer';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { render } from 'enzyme';
 import SiteNav from '.';
-import { menu } from './index.story';
+import { menu, menuProps } from './index.story';
 
 
 describe('<SiteNav>', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<Router><SiteNav menu={menu}>Hello World</SiteNav></Router>)
+      .create(<Router><SiteNav menu={menu} menuProps={menuProps}>Hello World</SiteNav></Router>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('has sidebar', () => {
-    const wrapper = render(<Router><SiteNav menu={menu}>Hello World</SiteNav></Router>);
+    const wrapper = render(<Router>
+        <SiteNav menu={menu} menuProps={menuProps}>Hello World</SiteNav>
+      </Router>);
     expect(wrapper.hasClass('pushable')).toBe(true);
     expect(wrapper.find('.sidebar').length).toBe(1);
   });
