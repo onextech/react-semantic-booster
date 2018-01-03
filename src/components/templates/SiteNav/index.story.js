8a0da05b3e05f289f111d2c2c829f38a17b19a0a
clodal <joel.koh@me.com>
2018-01-03 17:35:54 +0800

    update tests

--- src/components/templates/SiteNav/index.story.js
+++ src/components/templates/SiteNav/index.story.js
@@ -9,7 +9,14 @@ export const menu = [ // eslint-disable-line import/prefer-default-export
   {
     position: 'left',
     content: [
-      { name: 'Logo', to: '/', image: { height: 60, src: 'https://s3-ap-southeast-1.amazonaws.com/storage.api.merrymaker.com/images/merrymaker_mascot_head_side.svg' } },
+      {
+        name: 'Logo',
+        to: '/',
+        image: {
+          height: 50,
+          src: 'https://s3-ap-southeast-1.amazonaws.com/storage.api.merrymaker.com/images/merrymaker_mascot_head_side.svg',
+        },
+      },
       { name: 'Home', to: '/home' },
       { name: 'About', to: '/about' },
     ],
