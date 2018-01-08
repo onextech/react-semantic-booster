aa2866f41b757e97fe3ae066aa387d97a8bab06a
clodal <joel.koh@me.com>
2018-01-08 16:58:17 +0800

    enable Block padding to be affected by props

--- src/components/atoms/Block/index.js
+++ src/components/atoms/Block/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Segment } from 'semantic-ui-react';
 
-const PaddedSegment = styled(Segment)`
-  &.block.ui.segment {
-    padding: 4rem 0rem;
-    &.attached {
-      border: 0;
-    }
-  }
-`;
 
-const Block = ({ ...props }) => (<PaddedSegment className="block" {...props} />);
+const PaddedSegment = styled(Segment)`
+  &.ui.segment {
+    ${(props) => {
+    const baseEmPadding = 5;
+    return `padding: ${baseEmPadding * props.spacer}em 0`;
+  }}
+  }
+  &.ui.segment.attached { border: 0; }
+  `;
+
+const Block = ({ ...props }) => (<PaddedSegment {...props} />);
+
+Block.propTypes = {
+  spacer: PropTypes.number,
+};
+
+Block.defaultProps = {
+  spacer: 1,
+};
 
 export default Block;
--- src/components/atoms/Block/index.story.js
+++ src/components/atoms/Block/index.story.js
@@ -10,6 +10,13 @@ storiesOf('Block', module)
   .add('Stacked', () => (
     <div>
       <Block attached textAlign="center">Hello World</Block>
-      <Block attached inverted textAlign="center">Hello World</Block>
+      <Block attached textAlign="center">Hello World</Block>
+    </div>
+  ))
+  .add('Spacer', () => (
+    <div>
+      <Block attached secondary textAlign="center" spacer={1}>Hello World</Block>
+      <Block attached textAlign="center" spacer={2}>Hello World</Block>
+      <Block attached inverted textAlign="center" spacer={3}>Hello World</Block>
     </div>
   ));
