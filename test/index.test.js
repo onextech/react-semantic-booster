02ab4269c2a27e1056e59a3fe2e48c60d49af5e3
clodal <joel.koh@me.com>
2018-02-07 23:43:32 +0800

    compile in es6

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { render } from 'enzyme';
 import { sayHello, Block, MediaCss, Desktop } from '../dist';
+import { sayHello as sayHelloEs } from '../dist/es';
 
 
 describe('Distribution', () => {
   it('imports functions', () => {
     expect(sayHello()).toBe('Hello, Haz!');
     expect(sayHello('foo')).toBe('Hello, foo!');
+  });
+
+  it('imports functions via es6', () => {
+    expect(sayHelloEs()).toBe('Hello, Haz!');
+    expect(sayHelloEs('foo')).toBe('Hello, foo!');
   });
 
   it('imports React component', () => {
