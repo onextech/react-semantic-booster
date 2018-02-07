c2a0bb711f7a643ca35e2aa8958ae955dd0ea515
clodal <joel.koh@me.com>
2018-02-08 02:33:30 +0800

    testing loader issue

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { sayHello, Block, MediaCss, Desktop } from '../dist';
+import { sayHello, Block } from '../dist';
 
 
 describe('Distribution', () => {
@@ -14,8 +14,8 @@ describe('Distribution', () => {
     expect(wrapper.hasClass('ui segment')).toBe(true);
   });
 
-  it('imports responsive utils', () => {
-    expect(MediaCss).toBeTruthy();
-    expect(Desktop).toBeTruthy();
-  });
+  // it('imports responsive utils', () => {
+  //   expect(MediaCss).toBeTruthy();
+  //   expect(Desktop).toBeTruthy();
+  // });
 });
