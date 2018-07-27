d6fb1dccad95643d0d7420f3c046923d76023092
clodal <joel.koh@me.com>
2018-07-27 20:06:20 +0800

    export dropdownlink

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export const sayHello = (name: string = 'Haz'): string => `Hello, ${name}!`;
 // atoms
 export { default as ButtonLink } from './components/atoms/ButtonLink';
 export { default as MenuLink } from './components/atoms/MenuLink';
+export { default as DropdownLink } from './components/atoms/DropdownLink';
 export { default as Block } from './components/atoms/Block';
 export { default as Breadcrumbs } from './components/atoms/Breadcrumbs';
 export { default as LineAccordion } from './components/atoms/LineAccordion';
